c062468e02eb75b7fed0264eb25956fe39b03d61
Asabeneh.X.Yetayeh <asabeneh.yetayeh@blueprintgenetics.com>
2022-08-05 14:32:52 +0300

    some changes

--- create-fullstack-app.sh
+++ create-fullstack-app.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-name=$1
-$(mkdir $name)
-path="$(pwd)/$name"
-echo $path
-$(cd $path && mkdir client server)
-$(cd "$path/client" && npx create-react-app . && npm install axios redux react-redux redux-thunk react-router-dom bootstrap moment react-moment styled-components react-helmet classnames jwt-decode react-social-icons react-confetti)
-$(cd "$path/server" && mkdir controllers models routes utils validation && npm init -y && touch server.js)
-$(cd "$path/server" && npm install axios bcryptjs moment express mongoose mysql dotenv jsonwebtoken gravatar cors)
+# name=$1
+# $(mkdir $name)
+# path="$(pwd)/$name"
+# echo $path
+# $(cd $path && mkdir client server)
+# $(cd "$path/client" && npx create-react-app . && npm install axios redux react-redux redux-thunk react-router-dom bootstrap moment react-moment styled-components react-helmet classnames jwt-decode react-social-icons react-confetti)
+# $(cd "$path/server" && mkdir controllers models routes utils validation && npm init -y && touch server.js)
+# $(cd "$path/server" && npm install axios bcryptjs moment express mongoose mysql dotenv jsonwebtoken gravatar cors)
 green="\033[0;32m"
 clear="\033[0m"
 
 echo "${green}
 ==============================================================
-===============<^>  CONGRATULATIONS   <^>=====================
+============== ^_^  CONGRATULATIONS!   ^_^ ===================
 =============== Your app has been created ====================
 ===  Navigate the different folders in the fullstack app   ===
 ==============================================================
