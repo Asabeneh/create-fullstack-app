73a1eae8446a9312428743e3ae747ff26e88d15d
Asabeneh.X.Yetayeh <asabeneh.yetayeh@blueprintgenetics.com>
2022-08-12 15:47:30 +0300

    content added

--- create-fullstack-app.sh
+++ create-fullstack-app.sh
@@ -5,7 +5,7 @@ $(mkdir $name)
 path="$(pwd)/$name"
 echo $path
 $(cd $path && mkdir client server)
-$(cd "$path/client" && npx create-react-app . && npm install axios redux react-redux redux-thunk react-router-dom bootstrap moment react-moment styled-components react-helmet classnames jwt-decode react-social-icons react-confetti)
+$(cd "$path/client" && npx create-react-app . && npm install axios redux react-redux redux-thunk react-router-dom formik yup bootstrap moment react-moment styled-components react-helmet classnames jwt-decode react-social-icons react-confetti recharts)
 $(cd "$path/server" && mkdir controllers models routes utils validation && npm init -y && touch server.js)
 $(cd "$path/server" && npm install axios bcryptjs moment express mongoose mysql dotenv jsonwebtoken gravatar cors)
 green="\033[0;32m"
